ccb1838af18c9e0b53a0cd36032f10d6c538a464
Rory Acker <ackerr13@gmail.com>
2015-10-17 16:09:12 -0400

    Added seatNums to the divs

--- custom.js
+++ custom.js
@@ -3,9 +3,12 @@ window.onload = function(){
 
 var emp = document.getElementsByClassName("empty");
 //document.body.appendChild(emp);
+var seatNum=0; 
 for (var i=0;i < emp.length;i++){
         //addEvent(emp[i], 'click', createForm);
+        emp[i].setAttribute("data", seatNum+=1)
         emp[i].addEventListener("click",createForm);
+
     }
 
 //
@@ -15,6 +18,7 @@ function createForm(){
   var show = document.getElementsByTagName("form")[0];
   show.className= "show";
 }
+
 
 }
 
